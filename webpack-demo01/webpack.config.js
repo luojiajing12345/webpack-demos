b91d9013a8827d84280ede652293cdd387069d2d
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2018-08-06 18:24:24 +0800

    vue-demo

--- webpack-demo01/webpack.config.js
+++ webpack-demo01/webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
   }
 };
